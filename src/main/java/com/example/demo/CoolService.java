001bb5bc94d6f6d2e446dd12e3e37903be9c3c69
Moguianov, Anton <anton.moguianov@t-systems.com>
2020-04-20 16:21:32 +0300

    Change hello 12

--- src/main/java/com/example/demo/CoolService.java
+++ src/main/java/com/example/demo/CoolService.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 @Service
 public class CoolService {
 
-    static final String HELLO_STRING = "Hello!5";
+    static final String HELLO_STRING = "Hello!12";
 
     private static final String USER_REPRESENTATION_TEMPLATE = "<p>%s:%s</p>";
     private static final String NO_USERS_CREATED = "No users created.";
