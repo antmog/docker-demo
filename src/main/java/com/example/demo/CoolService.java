4f7f661c397373d43f1f14246d2ee76d0596f040
anton.moguyanov <anton.moguyanov@humans.net>
2020-11-30 18:21:03 +0300

    D-2314 Add very

--- src/main/java/com/example/demo/CoolService.java
+++ src/main/java/com/example/demo/CoolService.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 @Service
 public class CoolService {
 
-    static final String HELLO_STRING = "Hello! December is very soon!";
+    static final String HELLO_STRING = "Hello! World!";
 
     private static final String USER_REPRESENTATION_TEMPLATE = "<p>%s:%s</p>";
     private static final String NO_USERS_CREATED = "No users created.";
